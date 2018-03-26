8d464d7c8013fcf502a0885a90723a746357a2ea
Kentaro Sakae <musamusa0607@gmail.com>
2018-03-27 00:23:21 +0900

    Action作成

---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+import fetchJsonp from 'fetch-jsonp';
+import qs from 'qs';
+import ENV from '../../env.json';
+
+const API_URL = 'https://shopping.yahooapis.jp/ShoppingWebService/V1/json/categoryRanking';
+const APP_ID = ENV.CLIENT_ID;
+
+// リクエスト開始
+const startRequest = categoryId => ({
+  type: 'START_REQUEST',
+  payload: {
+    categoryId,
+  },
+});
+
+// レスポンス受信
+const receiveData = (categoryId, error, response) => ({
+  type: 'RECEIVE_DATA',
+  payload: {
+    categoryId,
+    error,
+    response,
+  },
+});
+
+// リクエスト完了
+const finishRequest = categoryId => ({
+  type: 'FINISH_REQUEST',
+  payload: {
+    categoryId,
+  },
+});
+
+// ランキングを取得
+const fetchRanking = categoryId => (
+  async (dispatch) => {
+    dispatch(startRequest(categoryId));
+
+    const queryString = qs.stringify({
+      appid: APP_ID,
+      category_id: categoryId,
+    });
+
+    try {
+      const response = await fetchJsonp(`${API_URL}?${queryString}`);
+      const data = await response.json();
+      dispatch(receiveData(categoryId, null, data));
+    } catch (err) {
+      dispatch(receiveData(categoryId, err));
+    }
+    dispatch(finishRequest(categoryId));
+  }
+);
+
+export default fetchRanking;
--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Ranking = ({ categoryId }) => (
-  <div>
-    <h2>Rankingコンポーネント</h2>
-    <p>カテゴリーID: {categoryId}</p>
-  </div>
-);
+export default class Ranking extends React.Component {
+  componentWillMount() {
+    this.props.onMount(this.props.categoryId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.categoryId !== nextProps.categoryId) {
+      this.props.onUpdate(nextProps.categoryId);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Rankingコンポーネント</h2>
+        <p>カテゴリーID: {this.props.categoryId}</p>
+      </div>
+    );
+  }
+}
 
 Ranking.propTypes = {
   categoryId: PropTypes.string,
+  onMount: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 Ranking.defaultProps = {
   // categoryId=1は総合ランキング
   categoryId: 1,
 };
-
-export default Ranking;
